0a923bd1e15f761fb91a45642a614e13a5cffb1a
Logan Absher <absherlogan@gmail.com>
2017-09-19 11:04:27 -0700

    logan-tuesday-login-and-actions

---
+++ src/actions/profile-actions.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import superagent from 'superagent';
+
+export const profileCreate = (profile) => ({
+  type: 'PROFILE_CREATE',
+  payload: profile
+});
+
+export const profileCreateRequest = (profile) => (dispatch) => {
+  let token = document.cookie.findHash;
+  return superagent.post('https://responsible-consumption-stagin.herokuapp.com/profile')
+  .set({Authorization: `Bearer ${token}`})
+  .send(profile)
+  .then((res) => {
+    dispatch(profileCreate(res.body))
+    return res;
+  });
+};
--- src/component/login-container/index.js
+++ src/component/login-container/index.js
@@ -1,28 +1,54 @@
 'use strict';
 
 import React from 'react';
+import {connect} from 'react-redux';
 
 import Dashboard from '../dashboard';
+import LoginForm from '../forms/login-form';
+import {signupRequest, signinRequest} from '../../actions/login-actions.js';
+import {getProfileRequest} from '../../actions/profile-actions.js';
 
 class LoginContainer extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: null,
+      hasError: null
     }
-  }
+    // this.handleLogin = this.handleLogin.bind(this);
+  };
 
-  // let handleRandomUserCreate = function(){
-  //
-  // }
+  componentWillMount(){
+    // if(document.cookie){
+    //   console.log('yes');
+    //   superagent.get(`https://responsible-consumption-stagin.herokuapp.com/api/profile`)
+    //   .set({Authorization: `Bearer ${document.cookie}`})
+    //   .end((err, res) => {
+    //     if(err){
+    //       this.setState({hasError: err.status});
+    //     }else{
+    //       this.setState({isLoggedIn: true});
+    //     }
+    //   });
+    // }else{
+    //   this.setState({isLoggedIn: false});
+    // }
+  };
 
   render(){
     return(
       <section>
-        <a href="">hi</a>
+        {this.state.hasError || !this.state.isLoggedIn ? <LoginForm /> : <Dashboard />}
       </section>
     )
   }
 };
 
-export default LoginContainer;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signupRequest: (user) => dispatch(signupRequest(user)),
+    signinRequest: (user) => dispatch(signinRequest(user))
+  }
+}
+
+export default connect(undefined, mapDispatchToProps)(LoginContainer);
